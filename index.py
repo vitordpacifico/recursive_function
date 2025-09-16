79f38f2b6a9954a52530b4a07f529793a1f38101
Victor Dias <victordias1124@gmail.com>
2025-09-16 17:30:59 -0300

    feat: função recursiva

---
+++ index.py
@@ -0,0 +1,15 @@
+def vence(n):
+    if n == 0:
+        return False
+    
+    for i in [1, 2, 3]:
+        if n - i >= 0:  
+            if not vence(n - i):
+                return True
+    
+    return False
+
+print(vence(4))   
+print(vence(5))   
+print(vence(7))   
+print(vence(8))   
